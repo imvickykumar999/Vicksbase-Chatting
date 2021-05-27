f8dd20730341e0ebd1ae2a484590cfb86bb8b653
imvickykumar999 <imvickykumar999@gmail.com>
2021-05-28 04:54:21 +0530

    pull

--- chatting/chat.py
+++ chatting/chat.py
@@ -1,8 +1,6 @@
-from multivicks import crud
+import crud
 
 obj1 = crud.vicks('@Hey_Vicks', name = 'Vicky')
-obj1.push()
-# obj1.push('kaisa hai tu ?')
+data = obj1.pull('Group/Chat')
 
-obj2 = crud.vicks('@Hey_Vicks')
-obj2.push()
+print(data)
