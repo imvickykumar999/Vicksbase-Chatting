f2f58f9b4517b91761a4a29088d9f631ece90f22
imvickykumar999 <imvickykumar999@gmail.com>
2021-05-28 00:22:38 +0530

    data json

--- chatting/chat.py
+++ chatting/chat.py
@@ -1,16 +1,40 @@
 from crud import Vicks as vix
 # print(help(h))
-from datetime import datetime
 
-obj = vix('https://chatting-c937e-default-rtdb.firebaseio.com/')
+from datetime import datetime
+# import time
+
+obj = vix('https://chatting-c937e-default-rtdb.firebaseio.com/', 'vicks')
 
 # f = obj.pull()
 # print(f)
 
-f = obj.push('Hello', 'Group/Chat')
-print(f)
+# dt = datetime.now()
+# d = str(dt).split()[0]
+# t = str(dt).split()[1].split('.')[0]
 
-dt = datetime.now()
-print(help(dt))
+# print(f'A/{dt}')
 
-# obj.remove('Hello')
+obj.save('Group/Chat/2021-05-28')
+
+while True:
+    dt = datetime.now()
+    d = str(dt).split()[0]
+    t = str(dt).split()[1].split('.')[0]
+
+    # name = input('\nEnter your name : ')
+    message = input('Enter your message : ')
+    # name = 'Vicks'
+
+    f = obj.push(f'{message}', f"Group/Chat/{d}/{t}@{name}")
+    # print(f)
+    obj.save()
+
+
+# print(obj.pull('Group/Chat'))
+# obj.save()
+# obj.remove(f"Group/Chat/{d}/23:50:45@{name}")
+
+# while(1):
+#     print(datetime.now())
+#     time.sleep(1)
--- chatting/crud.py
+++ chatting/crud.py
@@ -2,8 +2,17 @@
 # pip install imvickykumar999
 # C:\Users\Vicky\anaconda3\Lib\site-packages\vicksbase
 
+import json
+from datetime import datetime
+
+dt = datetime.now()
+d = str(dt).split()[0]
+t = str(dt).split()[1].split('.')[0]
+
 class Vicks:
-    def __init__(self, link):
+    def __init__(self,
+                link = 'https://chatting-c937e-default-rtdb.firebaseio.com/',
+                name = 'anonymous'):
 
         try:
             from vicksbase import firebase as f
@@ -18,17 +27,22 @@ class Vicks:
     def show(self):
         return self.link
 
-    def pull(self, child = 'A/B/C/Switch'):
+    def pull(self, child = 'Group/Chat'):
         result = self.firebase_obj.get(f'{child}', None)
         return result
 
-    def push(self, data = 1, child = 'A/B/C/Switch'):
+    def push(self, data = 1, child = 'Group/Chat'):
         self.firebase_obj.put('/', child, data)
         return self.pull(child = '/')
 
-    def remove(self, child = 'A/B/C/led2'):
+    def remove(self, child = 'A/B/C/led2'): # danger to run... loss of data.
         data = self.firebase_obj.delete('/', child)
         return self.pull(child = '/')
+
+    def save(self, child = 'Group/Chat'):
+        with open('data.json', 'w', encoding ='utf8') as json_file:
+            json.dump(self.pull(child), json_file, ensure_ascii = False)
+
 
 
 # link = 'https://chatting-c937e-default-rtdb.firebaseio.com/'
